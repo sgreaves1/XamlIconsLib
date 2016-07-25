290ceaac1d5d6c3b8183b902c837a6516f0e4dbf
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-07-25 14:04:13 +0100

    add 3d rotation, accessibility and face book messenger icons

---
+++ XamlIconLibrary/3DRotationIcon.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace XamlIconLibrary
+{
+    /// <summary>
+    /// Interaction logic for _3D_Rotation.xaml
+    /// </summary>
+    public partial class _3DRotationIcon
+    {
+        public Brush IconBorderColor
+        {
+            get { return (Brush)GetValue(IconBorderColorProperty); }
+            set { SetValue(IconBorderColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBorderColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBorderColorProperty =
+            DependencyProperty.Register("IconBorderColor", typeof(Brush), typeof(_3DRotationIcon), new PropertyMetadata(null));
+
+
+
+        public int IconBorderThickness
+        {
+            get { return (int)GetValue(IconBorderThicknessProperty); }
+            set { SetValue(IconBorderThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBorderThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBorderThicknessProperty =
+            DependencyProperty.Register("IconBorderThickness", typeof(int), typeof(_3DRotationIcon), new PropertyMetadata(0));
+        
+        public _3DRotationIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamlIconLibrary/Facebook-Messenger-Icon.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace XamlIconLibrary
+{
+    /// <summary>
+    /// Interaction logic for Facebook_Messenger_Icon.xaml
+    /// </summary>
+    public partial class Facebook_Messenger_Icon
+    {
+        public Brush IconBorderColor
+        {
+            get { return (Brush)GetValue(IconBorderColorProperty); }
+            set { SetValue(IconBorderColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBorderColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBorderColorProperty =
+            DependencyProperty.Register("IconBorderColor", typeof(Brush), typeof(Facebook_Messenger_Icon), new PropertyMetadata(null));
+
+
+
+        public int IconBorderThickness
+        {
+            get { return (int)GetValue(IconBorderThicknessProperty); }
+            set { SetValue(IconBorderThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBorderThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBorderThicknessProperty =
+            DependencyProperty.Register("IconBorderThickness", typeof(int), typeof(Facebook_Messenger_Icon), new PropertyMetadata(0));
+
+        public Facebook_Messenger_Icon()
+        {
+            InitializeComponent();
+        }
+    }
+}
