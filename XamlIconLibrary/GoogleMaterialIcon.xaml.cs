b9099dc3b3768fa696d0c0a792d52fa03e7fc116
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-07-26 14:44:09 +0100

    changed 3d rotation icon into material icon

---
+++ XamlIconLibrary/GoogleMaterialIcon.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows;
+using System.Windows.Media;
+namespace XamlIconLibrary
+{
+    /// <summary>
+    /// Interaction logic for MaterialIcon.xaml
+    /// </summary>
+    public partial class GoogleMaterialIcon
+    {
+        /// <summary>
+        /// Dependency Property used to back the <see cref="Data"/> Property
+        /// </summary>
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data",
+                typeof(string),
+                typeof(GoogleMaterialIcon),
+                new PropertyMetadata(""));
+
+        /// <summary>
+        /// Dependency Property used to back the <see cref="Icon"/> Property
+        /// </summary>
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", 
+                typeof(Icon), 
+                typeof(GoogleMaterialIcon), 
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Dependency Property used to back the <see cref="IconBorderColor"/> Property
+        /// </summary>
+        public static readonly DependencyProperty IconBorderColorProperty =
+            DependencyProperty.Register("IconBorderColor", 
+                typeof(Brush), 
+                typeof(GoogleMaterialIcon), 
+                new PropertyMetadata(Brushes.Black));
+
+        /// <summary>
+        /// Dependency Property used to back the <see cref="IconBorderThickness"/> Property
+        /// </summary>
+        public static readonly DependencyProperty IconBorderThicknessProperty =
+            DependencyProperty.Register("IconBorderThickness",
+                typeof(int), 
+                typeof(GoogleMaterialIcon), 
+                new PropertyMetadata(0));
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GoogleMaterialIcon()
+        {
+            InitializeComponent();
+
+            Data = IconHelper.UpdateData(Icon);
+        }
+        
+        /// <summary>
+        /// Select a predefined icon to use 
+        /// </summary>
+        public Icon Icon
+        {
+            get { return (Icon)GetValue(IconProperty); }
+            set
+            {
+                SetValue(IconProperty, value);
+                Data = IconHelper.UpdateData(Icon);
+            }
+        }
+
+        /// <summary>
+        /// Can be used to overwrite with your own icon data
+        /// </summary>
+        public string Data
+        {
+            get { return (string)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        /// <summary>
+        /// Property used to set the icons border colour
+        /// </summary>
+        public Brush IconBorderColor
+        {
+            get { return (Brush)GetValue(IconBorderColorProperty); }
+            set { SetValue(IconBorderColorProperty, value); }
+        }
+
+        /// <summary>
+        /// Property used to set the icons border thickness
+        /// </summary>
+        public int IconBorderThickness
+        {
+            get { return (int)GetValue(IconBorderThicknessProperty); }
+            set { SetValue(IconBorderThicknessProperty, value); }
+        }
+    }
+}
