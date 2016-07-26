f314f851cd7281ba3098c69d640f2a7715eadc1e
Sam Greaves <samgreaves0@gmail.com>
2016-07-26 07:47:59 +0100

    add account box icon

---
+++ XamlIconLibrary/AccountBox.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamlIconLibrary
+{
+    /// <summary>
+    /// Interaction logic for AccountBox.xaml
+    /// </summary>
+    public partial class AccountBox : UserControl
+    {
+        public Brush IconBorderColor
+        {
+            get { return (Brush)GetValue(IconBorderColorProperty); }
+            set { SetValue(IconBorderColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBorderColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBorderColorProperty =
+            DependencyProperty.Register("IconBorderColor", typeof(Brush), typeof(AccountBox), new PropertyMetadata(null));
+
+
+
+        public int IconBorderThickness
+        {
+            get { return (int)GetValue(IconBorderThicknessProperty); }
+            set { SetValue(IconBorderThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBorderThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBorderThicknessProperty =
+            DependencyProperty.Register("IconBorderThickness", typeof(int), typeof(AccountBox), new PropertyMetadata(0));
+
+        public AccountBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
