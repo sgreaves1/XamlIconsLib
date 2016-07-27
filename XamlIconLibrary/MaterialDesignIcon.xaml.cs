f93782071bdcc10fec604ceebe4bbf38096e7fd2
Sam Greaves <samgreaves0@gmail.com>
2016-07-27 07:31:53 +0100

    add author and source to material design icons

--- XamlIconLibrary/MaterialDesignIcon.xaml.cs
+++ XamlIconLibrary/MaterialDesignIcon.xaml.cs
@@ -56,6 +56,33 @@ namespace XamlIconLibrary
                 new PropertyMetadata(0));
 
         /// <summary>
+        /// Dependency Property used to back the <see cref="Author"/> Property
+        /// </summary>
+        public static readonly DependencyProperty AuthorProperty =
+            DependencyProperty.Register("Author",
+                typeof(string),
+                typeof(MaterialDesignIcon), 
+                new PropertyMetadata(""));
+
+        /// <summary>
+        /// Dependency Property used to back the <see cref="Source"/> Property
+        /// </summary>
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source",
+                typeof(string),
+                typeof(MaterialDesignIcon),
+                new PropertyMetadata(""));
+
+        /// <summary>
+        /// Dependency Property used to back the <see cref="ShowCredits"/> Property
+        /// </summary>
+        public static readonly DependencyProperty ShowCreditsProperty =
+            DependencyProperty.Register("ShowCredits",
+                typeof(bool),
+                typeof(MaterialDesignIcon),
+                new PropertyMetadata(false));
+
+        /// <summary>
         /// Constructor
         /// </summary>
         public MaterialDesignIcon()
@@ -97,6 +124,33 @@ namespace XamlIconLibrary
         {
             get { return (int)GetValue(IconBorderThicknessProperty); }
             set { SetValue(IconBorderThicknessProperty, value); }
+        }
+
+        /// <summary>
+        /// Property used to set the icons author name
+        /// </summary>
+        public string Author
+        {
+            get { return (string)GetValue(AuthorProperty); }
+            private set { SetValue(AuthorProperty, value); }
+        }
+
+        /// <summary>
+        /// Property used to set the location the icon came from
+        /// </summary>
+        public string Source
+        {
+            get { return (string)GetValue(SourceProperty); }
+            private set { SetValue(SourceProperty, value); }
+        }
+
+        /// <summary>
+        /// If true shows author and source
+        /// </summary>
+        public bool ShowCredits
+        {
+            get { return (bool)GetValue(ShowCreditsProperty); }
+            set { SetValue(ShowCreditsProperty, value); }
         }
     }
 }
