d3219c4947b276ba74bb8452b90f3992b1912695
Sam Greaves <samgreaves0@gmail.com>
2016-07-28 21:02:29 +0100

    add 4 icons

--- XamlIconLibrary/Xamalot/Tango_x_office_calender.xaml.cs
+++ XamlIconLibrary/Xamalot/Tango_x_office_calender.xaml.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public partial class Tango_x_office_calender 
     {
-        internal Tango_x_office_calender()
+        public Tango_x_office_calender()
         {
             InitializeComponent();
         }
